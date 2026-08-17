d382046e3b68a809c20d8faf01fb6728c1e6f04c
AkaOxSama <enriquebadoy@gmail.com>
2026-08-17 18:32:24 +0200

    CH2.Nodes: https://www.boot.dev/lessons/cdae7fca-a7dc-4706-b2c5-7a03d66db1c9

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import * 
+
+
+def main():
+   dummy_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+   print(dummy_node)
+
+if __name__ == "__main__":
+    main()
