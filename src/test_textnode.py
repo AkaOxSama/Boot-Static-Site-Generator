503bc7e565cb7ccb6e538317b7a10d18d4d4f047
AkaOxSama <enriquebadoy@gmail.com>
2026-08-20 18:21:02 +0200

    CH2. Nodes: https://www.boot.dev/lessons/80ddb6c5-8324-4850-a28c-0c6207596857

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import *
+from htmlnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -33,6 +34,26 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("Image text", TextType.IMAGE, "www.artstation.com")
         self.assertNotEqual(node, node2)
 
+    # Test textnode to htmlnode
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("this is very bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "this is very bold")
+
+    def test_link(self):
+        node = TextNode("this is a fishy link", TextType.LINK, "www.goggle.ia")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this is a fishy link")
+        self.assertEqual(html_node.props, {"href": "www.goggle.ia"})
 
 if __name__ == "__main__":
     unittest.main()
